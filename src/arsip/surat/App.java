930da396408f784da7acd577e648f11b4a9546ec
Muhammad Rosyid Izzulkhaq <rosyid.iz.rsd@gmail.com>
2020-06-05 13:42:53 +0700

    Update App

--- src/arsip/surat/App.java
+++ src/arsip/surat/App.java
@@ -1,15 +1,72 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2020 Universitas Teknologi Yogyakarta.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package arsip.surat;
 
+import arsip.surat.view.LoginView;
+import com.sun.java.swing.plaf.windows.WindowsLookAndFeel;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 /**
- *
- * @author Rosyid Iz
+ * Sistem Pengarsipan Surat Keluar dan Surat Masuk
+ * 
+ * @author Elvina Pandan Malik
+ * @author Taufik Ismail
+ * @author Muhammad Rosyid Izzulkhaq
+ * @author Putri Akhsadini
  */
-public class App {
+public class App implements Runnable {
+    
+    private final LoginView loginView;
+
+    public App() {
+        loginView = new LoginView();
+    }
+    
+    public void start() {
+        SwingUtilities.invokeLater(this);
+    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }   
+        try {
+            UIManager.setLookAndFeel(new WindowsLookAndFeel());
+        } catch (UnsupportedLookAndFeelException ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            App app = new App();
+            app.start();
+        }
+    }
+
+    @Override
+    public void run() {
+        loginView.setVisible(true);
+    }
 }
