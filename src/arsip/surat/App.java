077c76a2e709a7dc8fbe11d070d29f8c610e9d1e
Muhammad Rosyid Izzulkhaq <rosyid.iz.rsd@gmail.com>
2020-06-05 12:33:56 +0700

    Initial Commit

---
+++ src/arsip/surat/App.java
@@ -0,0 +1,15 @@
+package arsip.surat;
+
+/**
+ *
+ * @author Rosyid Iz
+ */
+public class App {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }   
+}
